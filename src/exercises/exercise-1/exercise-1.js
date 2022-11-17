72ca1a0d8201a97fb61f8d632971c0bfa95ecde0
Ana María Brazquez <abrazquez@deloitte.es>
2022-11-17 17:03:56 +0100

    errata enunciado

--- src/exercises/exercise-1/exercise-1.js
+++ src/exercises/exercise-1/exercise-1.js
@@ -1,6 +1,6 @@
-// Tenemos que crear una función que compruebe que dada una ciudad cualquiera,
-// compruebe que pertenece una comunidad autónoma (lista de ciudades)
-// Si no se recibe uno de los parámetros, se deberá volver false
+/* Tenemos que crear una función que dada una ciudad cualquiera,
+compruebe que pertenece a una comunidad autónoma (lista de ciudades).
+Si no se recibe uno de los parámetros, se deberá volver false */
 
 const isInCommunity = (city, community) => {
   return community?.includes(city);
